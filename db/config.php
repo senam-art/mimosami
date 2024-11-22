27a9b4b07dbcfbe468d56fd344758c112aaf7100
maisybaer <maisy.baer@ashesi.edu.gh>
2024-11-22 11:22:19 +0000

    update config

--- db/config.php
+++ db/config.php
@@ -1,19 +1,19 @@
 <?php
-// Set the content type to JSON
-header("Content-Type: application/json");
 
-// Database configuration
-$servername = "169.239.251.102"; // Remote server IP address
-$port = "3306"; // MySQL default port (you can specify it explicitly if required)
-$username = "maisy.baer"; // Your database username
-$password = "smarty8Aa.g@"; // Your database password
-$dbname = "webtech_fall2024_maisy_baer"; // Your database name
+$servername = "localhost";
+$username = "maisy.baer";
+$password = "smarty8Aa.g@";
+$dbname = "webtech_fall2024_maisy_baer";
 
-try {
-    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully!";
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    echo "Connection failed: " . $conn->connect_error;
+} else {
+    echo "Connection successful!";
 }
+
+
 ?>
